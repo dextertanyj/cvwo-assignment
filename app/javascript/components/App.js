8fbc71e97b7e9b88e840151a8f5cb0b43df7e010
Dexter <dextertanyj@gmail.com>
2020-01-05 15:00:39 +0800

    Relocated import of CSS

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -8,8 +8,6 @@ import ManageCategory from "./ManageCategory"
 import EditCategory from "./EditCategory";
 import SearchTodo from "./SearchTodo";
 import { Menu, Icon, Container } from 'semantic-ui-react';
-import 'semantic-ui-css/semantic.min.css';
-
 
 function App() {
 	return (
