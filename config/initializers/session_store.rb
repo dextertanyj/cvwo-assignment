93316905f5c7cba780ea96d6a13917c4ea551834
Dexter <dextertanyj@gmail.com>
2020-01-15 12:50:50 +0800

    Attempt Fix Cookie Store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,8 @@
 if Rails.env == "production"
     Rails.application.config.session_store :cookie_store, 
-        key: "_todolist_app", 
-        domain: "http://quiet-garden-09538.herokuapp.com/"
+        key: "_todolist_session", 
+        domain: "quiet-garden-09538.herokuapp.com"
 else
     Rails.application.config.session_store :cookie_store, 
-        key: "_todolist_app"
+        key: "_todolist_session"
 end
