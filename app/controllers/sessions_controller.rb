d2d8148d6545146339e6393b3c40dcbe4528d24f
Dexter <dextertanyj@gmail.com>
2020-02-07 21:33:58 +0800

    Attempt to fix logout button

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -30,8 +30,8 @@ class SessionsController < ApplicationController
     end
 
     def logout
-        session[:user_id] = nil
         @current_user = nil
+        session.clear
         render json: {
             status: 200,
             logged_out: true
